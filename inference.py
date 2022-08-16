fixed error in normalisation, added visibility also into dataset

inference.py
@@ -2,7 +2,7 @@ from keras.models import load_model
 import cv2
 import mediapipe as mp
 import numpy as np
-from utils import NormPoseLandmark
+from utils import NormPoseLandmark, min_max
 import pandas as pd
 
 
@@ -70,6 +70,8 @@ if result.pose_landmarks:
     # print(landmarks[0])
 
     data = pd.DataFrame(landmarks, columns=col_names)
+    data.to_csv('min_max_df.csv', encoding='utf-8', index=False)
+    # data = min_max(data)
     predict = model.predict(data)[0]
     print('prediction: ', predict)
     # class_id = np.argmax(predict, axis = 1)

poseLandmark_csv.py
@@ -5,7 +5,7 @@ import glob
 import pandas as pd
 import argparse
 import numpy as np
-from utils import NormPoseLandmark
+import math
 
 
 ap = argparse.ArgumentParser()
@@ -21,27 +21,27 @@ path_data_dir = args["dataset"]
 path_to_save = args["save"]
 
 ##############
-torso_size_multiplier=2.5
+torso_size_multiplier = 2.5
 n_landmarks = 33
 n_dimensions = 3
 landmark_names = [
-        'nose',
-        'left_eye_inner', 'left_eye', 'left_eye_outer',
-        'right_eye_inner', 'right_eye', 'right_eye_outer',
-        'left_ear', 'right_ear',
-        'mouth_left', 'mouth_right',
-        'left_shoulder', 'right_shoulder',
-        'left_elbow', 'right_elbow',
-        'left_wrist', 'right_wrist',
-        'left_pinky_1', 'right_pinky_1',
-        'left_index_1', 'right_index_1',
-        'left_thumb_2', 'right_thumb_2',
-        'left_hip', 'right_hip',
-        'left_knee', 'right_knee',
-        'left_ankle', 'right_ankle',
-        'left_heel', 'right_heel',
-        'left_foot_index', 'right_foot_index',
-    ]
+    'nose',
+    'left_eye_inner', 'left_eye', 'left_eye_outer',
+    'right_eye_inner', 'right_eye', 'right_eye_outer',
+    'left_ear', 'right_ear',
+    'mouth_left', 'mouth_right',
+    'left_shoulder', 'right_shoulder',
+    'left_elbow', 'right_elbow',
+    'left_wrist', 'right_wrist',
+    'left_pinky_1', 'right_pinky_1',
+    'left_index_1', 'right_index_1',
+    'left_thumb_2', 'right_thumb_2',
+    'left_hip', 'right_hip',
+    'left_knee', 'right_knee',
+    'left_ankle', 'right_ankle',
+    'left_heel', 'right_heel',
+    'left_foot_index', 'right_foot_index',
+]
 ##############
 
 mp_pose = mp.solutions.pose
@@ -56,11 +56,11 @@ for i in range(n_landmarks):
     name_x = name + '_X'
     name_y = name + '_Y'
     name_z = name + '_Z'
+    name_v = name + '_V'
     col_names.append(name_x)
     col_names.append(name_y)
     col_names.append(name_z)
-
-# print(col_list)
+    col_names.append(name_v)
 
 full_lm_list = []
 target_list = []
@@ -69,10 +69,12 @@ for class_name in class_list:
     img_list = glob.glob(path_to_class + '/*.jpg') + \
         glob.glob(path_to_class + '/*.jpeg') + \
         glob.glob(path_to_class + '/*.png')
+    img_list = sorted(img_list)
 
     # Read reach Images in the each classes
     for img in img_list:
         image = cv2.imread(img)
+        h, w, c = image.shape
         if image is None:
             print(
                 f'[ERROR] Error in reading {img} -- Skipping.....\n[INFO] Taking next Image')
@@ -82,26 +84,40 @@ for class_name in class_list:
             result = pose.process(img_rgb)
             if result.pose_landmarks:
                 lm_list = []
-                for lm in result.pose_landmarks.landmark:
-                    lm_list.append(lm.x)
-                    lm_list.append(lm.y)
-                    lm_list.append(lm.z)
+                for landmarks in result.pose_landmarks.landmark:
+                    # Preprocessing
+                    max_distance = 0
+                    lm_list.append(landmarks)
+                center_x = (lm_list[landmark_names.index('right_hip')].x +
+                            lm_list[landmark_names.index('left_hip')].x)*0.5
+                center_y = (lm_list[landmark_names.index('right_hip')].y +
+                            lm_list[landmark_names.index('left_hip')].y)*0.5
 
-                # Preprocessing
-                landmarks = np.array(lm_list, np.float32).reshape([n_landmarks, n_dimensions])
-                norm_landmarks = NormPoseLandmark(landmark_names, landmarks)
-                landmarks = np.array(norm_landmarks, np.float32).reshape([1, 99])
-                # print(landmarks[0])
+                shoulders_x = (lm_list[landmark_names.index('right_shoulder')].x +
+                               lm_list[landmark_names.index('left_shoulder')].x)*0.5
+                shoulders_y = (lm_list[landmark_names.index('right_shoulder')].y +
+                               lm_list[landmark_names.index('left_shoulder')].y)*0.5
 
-                full_lm_list.append(landmarks[0])
+                for lm in lm_list:
+                    distance = math.sqrt(
+                        (lm.x - center_x)**2 + (lm.y - center_y)**2)
+                    if(distance > max_distance):
+                        max_distance = distance
+                torso_size = math.sqrt(
+                    (shoulders_x - center_x)**2 + (shoulders_y - center_y)**2)
+                max_distance = max(
+                    torso_size*torso_size_multiplier, max_distance)
+
+                pre_lm = list(np.array([[(landmark.x-center_x)/max_distance, (landmark.y-center_y)/max_distance,
+                              landmark.z/max_distance, landmark.visibility] for landmark in lm_list]).flatten())
+
+                full_lm_list.append(pre_lm)
                 target_list.append(class_name)
-                
+
             print(f'{os.path.split(img)[1]} Landmarks added Successfully')
     print(f'[INFO] {class_name} Successfully Completed')
 
 print('[INFO] Landmarks from Dataset Successfully Completed')
-
-target_list = sorted(target_list)
 
 data_x = pd.DataFrame(full_lm_list, columns=col_names)
 data = data_x.assign(Pose_Class=target_list)