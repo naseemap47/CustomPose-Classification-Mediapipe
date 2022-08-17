a548b62937bab9126f12a1c7f084887dd86ef4c7
NASEEM A P <naseemalassampattil@gmail.com>
2022-08-17 11:13:34 +0530

    added web-cam and video into inference, added confidence for predictions

--- inference.py
+++ inference.py
@@ -8,15 +8,17 @@ import argparse
 
 
 ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--image", type=str, required=True,
-                help="path to sample image")
-
 ap.add_argument("-m", "--model", type=str, required=True,
                 help="path to saved .h5 model, eg: dir/model.h5")
+ap.add_argument("-c", "--conf", type=float, required=True,
+                help="min prediction conf to detect pose class (0<conf<1)")
+ap.add_argument("-i", "--source", type=str, required=True,
+                help="path to sample image")
 
 args = vars(ap.parse_args())
-path_to_img = args["image"]
+source = args["source"]
 path_saved_model = args["model"]
+threshold = args["conf"]
 
 ##############
 torso_size_multiplier = 2.5
@@ -64,47 +66,118 @@ for i in range(n_landmarks):
 # Load saved model
 model = load_model(path_saved_model, compile=True)
 
-# Load sample Image
-img = cv2.imread(path_to_img)
-img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-result = pose.process(img_rgb)
-if result.pose_landmarks:
-    lm_list = []
-    for landmarks in result.pose_landmarks.landmark:
-        # Preprocessing
-        max_distance = 0
-        lm_list.append(landmarks)
-    center_x = (lm_list[landmark_names.index('right_hip')].x +
-                lm_list[landmark_names.index('left_hip')].x)*0.5
-    center_y = (lm_list[landmark_names.index('right_hip')].y +
-                lm_list[landmark_names.index('left_hip')].y)*0.5
+if source.endswith(('.jpg', '.jpeg', '.png')):
+    path_to_img = source
+    # Load sample Image
+    img = cv2.imread(path_to_img)
+    img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    result = pose.process(img_rgb)
+    if result.pose_landmarks:
+        lm_list = []
+        for landmarks in result.pose_landmarks.landmark:
+            # Preprocessing
+            max_distance = 0
+            lm_list.append(landmarks)
+        center_x = (lm_list[landmark_names.index('right_hip')].x +
+                    lm_list[landmark_names.index('left_hip')].x)*0.5
+        center_y = (lm_list[landmark_names.index('right_hip')].y +
+                    lm_list[landmark_names.index('left_hip')].y)*0.5
 
-    shoulders_x = (lm_list[landmark_names.index('right_shoulder')].x +
-                   lm_list[landmark_names.index('left_shoulder')].x)*0.5
-    shoulders_y = (lm_list[landmark_names.index('right_shoulder')].y +
-                   lm_list[landmark_names.index('left_shoulder')].y)*0.5
+        shoulders_x = (lm_list[landmark_names.index('right_shoulder')].x +
+                       lm_list[landmark_names.index('left_shoulder')].x)*0.5
+        shoulders_y = (lm_list[landmark_names.index('right_shoulder')].y +
+                       lm_list[landmark_names.index('left_shoulder')].y)*0.5
 
-    for lm in lm_list:
-        distance = math.sqrt((lm.x - center_x)**2 + (lm.y - center_y)**2)
-        if(distance > max_distance):
-            max_distance = distance
-    torso_size = math.sqrt((shoulders_x - center_x) **
-                           2 + (shoulders_y - center_y)**2)
-    max_distance = max(torso_size*torso_size_multiplier, max_distance)
+        for lm in lm_list:
+            distance = math.sqrt((lm.x - center_x)**2 + (lm.y - center_y)**2)
+            if(distance > max_distance):
+                max_distance = distance
+        torso_size = math.sqrt((shoulders_x - center_x) **
+                               2 + (shoulders_y - center_y)**2)
+        max_distance = max(torso_size*torso_size_multiplier, max_distance)
 
-    pre_lm = list(np.array([[(landmark.x-center_x)/max_distance, (landmark.y-center_y)/max_distance,
-                  landmark.z/max_distance, landmark.visibility] for landmark in lm_list]).flatten())
-    data = pd.DataFrame([pre_lm], columns=col_names)
-    predict = model.predict(data)[0]
-    print('predictions: ', predict)
-    print('predicted Pose Class: ', class_names[predict.argmax()])
+        pre_lm = list(np.array([[(landmark.x-center_x)/max_distance, (landmark.y-center_y)/max_distance,
+                                 landmark.z/max_distance, landmark.visibility] for landmark in lm_list]).flatten())
+        data = pd.DataFrame([pre_lm], columns=col_names)
+        predict = model.predict(data)[0]
+        if max(predict) > threshold:
+            pose_class = class_names[predict.argmax()]
+            print('predictions: ', predict)
+            print('predicted Pose Class: ', pose_class)
+        else:
+            pose_class = 'Unknown Pose'
+            print('[INFO] Predictions is below given Confidence!!')
 
-# Show Result
-img = cv2.putText(
-    img, f'{class_names[predict.argmax()]}',
-    (40, 50), cv2.FONT_HERSHEY_PLAIN,
-    2, (255, 0, 255), 2
-)
-cv2.imshow('Output Image', img)
-if cv2.waitKey(0) & 0xFF == ord('q'):
-    cv2.destroyAllWindows()
+    # Show Result
+    img = cv2.putText(
+        img, f'{class_names[predict.argmax()]}',
+        (40, 50), cv2.FONT_HERSHEY_PLAIN,
+        2, (255, 0, 255), 2
+    )
+    cv2.imshow('Output Image', img)
+    if cv2.waitKey(0) & 0xFF == ord('q'):
+        cv2.destroyAllWindows()
+    print('[INFO] Inference on Test Image is Ended...')
+
+else:
+    # Web-cam
+    if source.isnumeric():
+        source = int(source)
+
+    cap = cv2.VideoCapture(source)
+    while True:
+        success, img = cap.read()
+        if not success:
+            print('[ERROR] Failed to Read Video feed')
+            break
+        img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        result = pose.process(img_rgb)
+
+        if result.pose_landmarks:
+            lm_list = []
+            for landmarks in result.pose_landmarks.landmark:
+                # Preprocessing
+                max_distance = 0
+                lm_list.append(landmarks)
+            center_x = (lm_list[landmark_names.index('right_hip')].x +
+                        lm_list[landmark_names.index('left_hip')].x)*0.5
+            center_y = (lm_list[landmark_names.index('right_hip')].y +
+                        lm_list[landmark_names.index('left_hip')].y)*0.5
+
+            shoulders_x = (lm_list[landmark_names.index('right_shoulder')].x +
+                           lm_list[landmark_names.index('left_shoulder')].x)*0.5
+            shoulders_y = (lm_list[landmark_names.index('right_shoulder')].y +
+                           lm_list[landmark_names.index('left_shoulder')].y)*0.5
+
+            for lm in lm_list:
+                distance = math.sqrt((lm.x - center_x) **
+                                     2 + (lm.y - center_y)**2)
+                if(distance > max_distance):
+                    max_distance = distance
+            torso_size = math.sqrt((shoulders_x - center_x) **
+                                   2 + (shoulders_y - center_y)**2)
+            max_distance = max(torso_size*torso_size_multiplier, max_distance)
+
+            pre_lm = list(np.array([[(landmark.x-center_x)/max_distance, (landmark.y-center_y)/max_distance,
+                                     landmark.z/max_distance, landmark.visibility] for landmark in lm_list]).flatten())
+            data = pd.DataFrame([pre_lm], columns=col_names)
+            predict = model.predict(data)[0]
+            if max(predict) > threshold:
+                pose_class = class_names[predict.argmax()]
+                print('predictions: ', predict)
+                print('predicted Pose Class: ', pose_class)
+            else:
+                pose_class = 'Unknown Pose'
+                print('[INFO] Predictions is below given Confidence!!')
+
+            # Show Result
+            img = cv2.putText(
+                img, f'{pose_class}',
+                (40, 50), cv2.FONT_HERSHEY_PLAIN,
+                2, (255, 0, 255), 2
+            )
+        cv2.imshow('Output Image', img)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            cv2.destroyAllWindows()
+            break
+    print('[INFO] Inference on Videostream is Ended...')
